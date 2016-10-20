c1cd892123d65a7d499c0cf8fcb1d677dfb1f4f8
RenatoRasic <renato.rasic0@gmail.com>
2016-10-20 17:12:30 +0200

    first commit

---
+++ php2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>NASLOV</title>
+        <meta charset="windows-1250">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Page one</h1>
+        <img src= "http://localhost/Static/winter-wallpaper-12.jpg"/>
+    </body>
+    <footer>
+        <p>date: <?php echo date('d.m.Y.') ?></p>
+    </footer>
+</html>
